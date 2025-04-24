e481fc9dde1fc16f180b2e7419308a5874c14041
kyshin <kyshin1993@gmail.com>
2025-04-24 22:02:36 +0200

    project initialization, copyright and author have been added

---
+++ src/labs/pm/data/Product.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2025.
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public Licence a published by the Free Software Foundation , either version 3 of the Licence , or (at your opinion) ant later version.
+ *
+ * This program is distributed in the hope that it will be useful but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY  or FITNESS FOR PRACTICAL PURPOSE. See the GNU General Public Licence for more details.
+ *
+ * You should have received a copy of the GNU General Public Licence along with this program. If not, see <http://www.gnu.org/licences/>.
+ */
+
+package labs.pm.data;
+
+import java.math.BigDecimal;
+
+/**
+ * @author kyshi
+ **/
+public class Product {
+    private int id;
+    private String name;
+    private BigDecimal price;
+}
