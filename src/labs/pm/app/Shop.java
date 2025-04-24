14eddf07bd3e74ad921f23d78be44e3b89e8d5e2
kyshin <kyshin1993@gmail.com>
2025-04-24 22:18:49 +0200

    practice4.2

--- src/labs/pm/app/Shop.java
+++ src/labs/pm/app/Shop.java
@@ -1,7 +1,17 @@
 package labs.pm.app;
 
+import labs.pm.data.Product;
+
+import java.math.BigDecimal;
+
 public class Shop {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Product p1 = new Product();
+        p1.setId(101);
+        p1.setName("Tea");
+        p1.setPrice(BigDecimal.valueOf(1.99));
+
+        System.out.println(p1.getId() + " " + p1.getName() + " " + p1.getPrice());
+
     }
 }
