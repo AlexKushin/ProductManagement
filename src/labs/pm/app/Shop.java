6ef3de82fc1ddd54a0a80b667b097803c57bbe86
kyshin <kyshin1993@gmail.com>
2025-04-26 21:04:27 +0200

    practice4.3

--- src/labs/pm/app/Shop.java
+++ src/labs/pm/app/Shop.java
@@ -1,4 +1,26 @@
+/*
+ * Copyright (c) 2025.
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public Licence as
+ * published by the Free Software Foundation , either version 3 of
+ * the Licence , or (at your opinion) ant later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY  or FITNESS FOR PRACTICAL PURPOSE. See the
+ * GNU General Public Licence for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * Licence along with this program. If not, see
+ * <http://www.gnu.org/licences/>.
+ */
 package labs.pm.app;
+
+/**
+ * {@code Shop} class represents an application that manages Products
+ * @version 4.0
+ * @author kyshi
+ **/
 
 import labs.pm.data.Product;
 
@@ -7,11 +29,11 @@ import java.math.BigDecimal;
 public class Shop {
     public static void main(String[] args) {
         Product p1 = new Product();
-        p1.setId(101);
-        p1.setName("Tea");
+        //p1.setId(101);
+        //p1.setName("Tea");
         p1.setPrice(BigDecimal.valueOf(1.99));
 
-        System.out.println(p1.getId() + " " + p1.getName() + " " + p1.getPrice());
+        System.out.println(p1.getId() + " " + p1.getName() + " " + p1.getPrice() + " " + p1.getDiscount());
 
     }
 }
--- src/labs/pm/data/Product.java
+++ src/labs/pm/data/Product.java
@@ -1,20 +1,41 @@
 /*
  * Copyright (c) 2025.
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public Licence a published by the Free Software Foundation , either version 3 of the Licence , or (at your opinion) ant later version.
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public Licence as
+ * published by the Free Software Foundation , either version 3 of
+ * the Licence , or (at your opinion) ant later version.
  *
- * This program is distributed in the hope that it will be useful but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY  or FITNESS FOR PRACTICAL PURPOSE. See the GNU General Public Licence for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY  or FITNESS FOR PRACTICAL PURPOSE. See the
+ * GNU General Public Licence for more details.
  *
- * You should have received a copy of the GNU General Public Licence along with this program. If not, see <http://www.gnu.org/licences/>.
+ * You should have received a copy of the GNU General Public
+ * Licence along with this program. If not, see
+ * <http://www.gnu.org/licences/>.
  */
 
 package labs.pm.data;
+/**
+ * {@code Product} class represents properties and behaviours of
+ * product objects in the Product Management System
+ * <br>
+ * Each product has an id, name and price
+ * <br>
+ * Each product can have a discount, calculated based on a
+ * {@link DISCOUNT_RATE discount rate}
+ *
+ * @version 4.0
+ * @author kyshi
+ **/
 
 import java.math.BigDecimal;
 
-/**
- * @author kyshi
- **/
+import static java.math.RoundingMode.HALF_UP;
+
+
 public class Product {
+    public static final BigDecimal DISCOUNT_RATE = BigDecimal.valueOf(0.1);
     private int id;
     private String name;
     private BigDecimal price;
@@ -39,7 +60,19 @@ public class Product {
         return price;
     }
 
-    public void setPrice(BigDecimal price) {
+    public void setPrice(final BigDecimal price) {
         this.price = price;
     }
+
+    /**
+     * Calculates discount based on a product price and
+     * {@link DISCOUNT_RATE discount rate}
+     *
+     * @return a {@link java.math.BigDecimal BigDecimal}
+     * value of the discount
+     */
+    public BigDecimal getDiscount() {
+        return price.multiply(DISCOUNT_RATE).setScale(2, HALF_UP);
+    }
 }
+
