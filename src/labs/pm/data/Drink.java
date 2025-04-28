17f756a67368359f1e8a2755398e9cf1f6ebd1b7
kyshin <kyshin1993@gmail.com>
2025-04-28 17:33:53 +0200

    practice6.2 -practice polymorphism override getDiscount() logic in subclasses

--- src/labs/pm/data/Drink.java
+++ src/labs/pm/data/Drink.java
@@ -10,6 +10,7 @@
 package labs.pm.data;
 
 import java.math.BigDecimal;
+import java.time.LocalTime;
 
 /**
  * @author kyshi
@@ -17,5 +18,14 @@ import java.math.BigDecimal;
 public class Drink extends Product {
     public Drink(int id, String name, BigDecimal price, Rating rating) {
         super(id, name, price, rating);
+    }
+
+    @Override
+    public BigDecimal getDiscount() {
+        LocalTime now = LocalTime.now();
+
+        return (now.isAfter(LocalTime.of(16, 30)) &&
+                now.isBefore(LocalTime.of(18, 30)))
+                ? super.getDiscount() : BigDecimal.ZERO;
     }
 }
