8b3b183ef4d85d899f6a6bd113664c52330c886b
kyshin <kyshin1993@gmail.com>
2025-04-30 19:55:52 +0200

    practice7.2 - Products Review and Rating, resource bundle has been created, method printProductReport() has been refactored

--- src/labs/pm/app/Shop.java
+++ src/labs/pm/app/Shop.java
@@ -29,36 +29,15 @@ import static labs.pm.data.Rating.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Locale;
 
 public class Shop {
     public static void main(String[] args) {
-        ProductManager pm = new ProductManager();
+        ProductManager pm = new ProductManager(Locale.US);
 
         Product p1 = pm.createProduct(101, "Tea", BigDecimal.valueOf(1.99), NOT_RATED);
-//        Product p2 = pm.createProduct(102, "Coffee", BigDecimal.valueOf(2.99), FOUR_STAR);
-//        Product p3 = pm.createProduct(103, "Cake", BigDecimal.valueOf(3.99), FIVE_STAR, LocalDate.now().plusDays(2));
-//        Product p4 = pm.createProduct(101, "Cookie", BigDecimal.valueOf(3.99), TWO_STAR, LocalDate.now());
-//        Product p5 = p3.applyRating(THREE_STAR);
-//        Product p6 = pm.createProduct(104, "Chocolate", BigDecimal.valueOf(2.99), FIVE_STAR);
-//        Product p7 = pm.createProduct(104, "Chocolate", BigDecimal.valueOf(2.99), FIVE_STAR, LocalDate.now().plusDays(2));
-//        Product p8 = p4.applyRating(FIVE_STAR);
-//        Product p9 = p1.applyRating(TWO_STAR);
-
         System.out.println(p1);
-//        System.out.println(p2);
-//        System.out.println(p3);
-//        System.out.println(p4);
-//        System.out.println(p5);
-//        System.out.println(p6);
-//        System.out.println(p7);
-//        System.out.println(p6.equals(p7));
-//        System.out.println(p8);
-//        System.out.println(p9);
-//        /*if(p3 instanceof Food f){
-//            System.out.println(f.getBestBefore());
-//        }*/
-//        System.out.println(p3.getBestBefore());
-//        System.out.println(p1.getBestBefore());
+
         pm.printProductReport();
         p1 = pm.reviewProduct(p1, FOUR_STAR, "Nice tea");
         pm.printProductReport();
